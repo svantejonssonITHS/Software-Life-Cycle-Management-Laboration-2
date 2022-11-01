5ff1207bf496b00e89aaa7d11c60616324a7981a
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-11-01 22:16:24 +0100

    Init LogRocket

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,14 @@
+// External dependencies
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import LogRocket from 'logrocket';
+
+// Internal dependencies
 import './index.css';
+import App from './App';
+
+// Initialize LogRocket
+LogRocket.init(import.meta.env.VITE_LOGROCKET_ID);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
