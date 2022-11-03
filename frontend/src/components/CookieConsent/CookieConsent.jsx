46fdca4438c80e9e0646a4cfc1277d264d9506fc
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-11-03 10:28:37 +0100

    add cookie consent styling

--- frontend/src/components/CookieConsent/CookieConsent.jsx
+++ frontend/src/components/CookieConsent/CookieConsent.jsx
@@ -9,20 +9,30 @@ function CookieConsent() {
 	useEffect(() => {
 		if (cookieConsent === 'true') {
 			// If the user has consented, save their choice in Session Storage and initialize LogRocket monitoring
-			sessionStorage.setItem('cookieConsent', true);
+			sessionStorage.setItem('cookieConsent', 'true');
 
 			LogRocket.init(import.meta.env.VITE_LOGROCKET_ID);
 		} else if (cookieConsent === 'false') {
 			// If the user has not consented, save their choice in Session Storage
-			sessionStorage.setItem('cookieConsent', false);
+			sessionStorage.setItem('cookieConsent', 'false');
 		}
 	}, [cookieConsent]);
 
 	if (cookieConsent === null) {
 		return (
-			<div>
-				<button onClick={() => setCookieConsent(true)}>CookieCookieCookie</button>
-				<button onClick={() => setCookieConsent(false)}>Dissent</button>
+			<div className="cookieConsent_outer-container">
+				<div className="cookieConsent_inner-container">
+					<h1>We use cookies</h1>
+					<p>We use cookies to improve your experience on our website. Do you consent to our use of cookies?</p>
+					<div className="cookieConsent_button-container">
+						<button className="consent" onClick={() => setCookieConsent('true')}>
+							I accept cookies
+						</button>
+						<button className="decline" onClick={() => setCookieConsent('false')}>
+							I decline cookies
+						</button>
+					</div>
+				</div>
 			</div>
 		);
 	}
