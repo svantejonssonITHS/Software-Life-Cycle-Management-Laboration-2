dacce7d7bc96eaef7febb1f15403d3a97d515397
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-11-03 09:14:21 +0100

    moved logrocket init to App.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,10 @@
 // External dependencies
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import LogRocket from 'logrocket';
 
 // Internal dependencies
 import './index.css';
 import App from './App';
-
-// Initialize LogRocket
-LogRocket.init(import.meta.env.VITE_LOGROCKET_ID);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
