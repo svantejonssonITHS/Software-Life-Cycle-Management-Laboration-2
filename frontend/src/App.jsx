1b60f968fd9fb61ff2c6c2c7e420fb00374181d0
Linus Romland <48645689+linusromland@users.noreply.github.com>
2022-11-04 15:23:31 +0100

    Added refresh button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,86 +2,81 @@
 import { useState, useEffect } from 'react';
 
 // Internal dependencies
-import reactLogo from './assets/react.svg';
+import refreshIcon from './assets/refresh.svg';
 import './App.css';
 import CookieConsent from './components/CookieConsent/CookieConsent.jsx';
 import Card from './components/Card';
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [input, setInput] = useState('');
+	const [posts, setPosts] = useState([]);
+	const [input, setInput] = useState('');
 
-  useEffect(() => {
-    fetch(import.meta.env.VITE_URL_BACKEND)
-      .then((Response) => Response.json())
-      .then((data) => {
-        setPosts(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+	useEffect(getPosts, []);
 
-  function handleSubmit() {
-    setPosts([
-      ...posts,
-      {
-        _id: Math.random().toString(36).substr(2, 9),
-        content: input,
-        createdAt: new Date().toISOString()
-      }
-    ]);
+	const BACKEND_URL = import.meta.env.VITE_URL_BACKEND || 'http://localhost:3000';
 
-    fetch(import.meta.env.VITE_URL_BACKEND, {
-      method: 'POST',
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ content: input })
-    })
-      .then((Response) => Response.json())
-      .then((data) => console.log(data));
-    setInput('');
-  }
+	/**
+	 * Fetches posts from the backend
+	 */
+	function getPosts() {
+		fetch(BACKEND_URL)
+			.then((Response) => Response.json())
+			.then((data) => {
+				setPosts(data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
 
-  return (
-    <main>
-      <CookieConsent />
-      <div>
-        <p className="inputTitle">Express your thoughts:</p>
-        <div className="inputWrapper">
-          <input
-            type="text"
-            placeholder="Write your thoughts here..."
-            className="input"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            onKeyDown={(e) => e.key == 'Enter' && handleSubmit()}
-          />
-          <button
-            className="inputButton"
-            onClick={handleSubmit}
-            disabled={input.length === 0}
-          >
-            Send
-          </button>
-        </div>
-      </div>
+	/**
+	 * Handles the input submit
+	 */
+	function handleSubmit() {
+		fetch(BACKEND_URL, {
+			method: 'POST',
+			mode: 'cors',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ content: input })
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data.message);
+				getPosts();
+				setInput('');
+			});
+	}
 
-      <div className="cards">
-        {posts.map((post) => {
-          return (
-            <Card
-              key={post._id}
-              content={post.content}
-              createdAt={post.createdAt}
-            />
-          );
-        })}
-      </div>
-    </main>
-  );
+	return (
+		<main>
+			<CookieConsent />
+			<div className='topBar'>
+				<div>
+					<p className='inputTitle'>Express your thoughts:</p>
+					<div className='inputWrapper'>
+						<input type='text' placeholder='Write your thoughts here...' className='input' value={input} onChange={(e) => setInput(e.target.value)} onKeyDown={(e) => e.key == 'Enter' && handleSubmit()} maxLength='35' />
+						<button className='inputButton' onClick={handleSubmit} disabled={input.length === 0}>
+							Send
+						</button>
+					</div>
+				</div>
+
+				<div>
+					<button className='refreshButton' onClick={getPosts}>
+						<img src={refreshIcon} alt='Refresh icon' />
+						Refresh
+					</button>
+				</div>
+			</div>
+			<div className='cards'>
+				{posts.map((post) => {
+					return <Card key={post._id} content={post.content} createdAt={post.createdAt} />;
+				})}
+			</div>
+		</main>
+	);
 }
 
 export default App;
